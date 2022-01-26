28ced921dabeea289578c415f23888aa9b4dfad4
eliezer <eliezer.c.alves2015@gmail.com>
2022-01-25 22:35:17 -0300

    [Aula-2] feat: importando usuarios do gihub / redirecinando para pagina do chat -> Trabalhando useStage e useRouter

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router'
 import appConfig from '../config.json'
-
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -46,11 +19,11 @@ function Title(props) {
     );
 }
 export default function PaginaInicial() {
-    const username = 'eliezer-alves';
+    const [username, setUsername] = React.useState();
+    const router = useRouter();
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -77,6 +50,10 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={(event) => {
+                event.preventDefault();
+                router.push('/chat')
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -88,6 +65,10 @@ export default function PaginaInicial() {
               </Text>
   
               <TextField
+                value={username}
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
